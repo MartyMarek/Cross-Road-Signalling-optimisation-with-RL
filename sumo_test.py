f06e117a9b4d6a12b1f27a68b9b2ef45b282100d
Zachary Kerr <Zachary.Kerr@bentley.com>
2022-05-12 23:13:10 +1000

    Rough sumo test python file update

--- sumo_test.py
+++ sumo_test.py
@@ -1,5 +1,5 @@
 import os, sys
-
+import pandas as pd
 
 # if 'SUMO_HOME' in os.environ:
 #     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -7,16 +7,72 @@ import os, sys
 # else:
 #     sys.exit("please declare environment variable 'SUMO_HOME'")
 
+# Running Sumo
 sys.path.append(os.path.join('c:', os.sep, 'Program Files (x86)', 'Eclipse', 'Sumo','tools'))
-
 sumoBinary = "C:\\Program Files (x86)\\Eclipse\\Sumo\\bin\\sumo-gui"
+
+# File to run
 sumoCmd = [sumoBinary, "-c", "_sumo\\simplest_intersection.sumocfg"]
 
 import traci
-traci.start(sumoCmd)
+traci.start(sumoCmd) # Need to press play in the GUI after this
+traci.simulation.start()
+traci.simulationStep()
+
+# Get all vehicles
+traci.vehicle.getIDList()
+# Get vehicle count
+traci.vehicle.getIDCount()
+
+vehicle_ids = traci.vehicle.getIDList()
+
+vehicles_df = pd.DataFrame()
+
+for vehicle_id in vehicle_ids:
+    
+    stopped_state = traci.vehicle.getStopState(vehID=vehicle_id) # I don't think stopped state means what we thought
+    waiting_time = traci.vehicle.getWaitingTime(vehID=vehicle_id)
+    accumulated_waiting_time = traci.vehicle.getAccumulatedWaitingTime(vehID=vehicle_id)
+    speed = traci.vehicle.getSpeed(vehID=vehicle_id)
+
+    vehicle_df = pd.DataFrame(
+        data=
+        [[
+            vehicle_id,
+            waiting_time,
+            accumulated_waiting_time,
+            stopped_state,
+            speed
+        ]],
+        columns=
+        [
+            "vehicle_id",
+            "waiting_time",
+            "accumulated_waiting_time",
+            "stopped_state",
+            "speed"
+        ]
+    )
+
+    vehicles_df = pd.concat(
+        [
+            vehicles_df,
+            vehicle_df
+        ],
+        ignore_index=True
+    )
+    
+
+vehicles_df
+
+traci.simulationStep()
 step = 0
+print("A")
+
+
 while step < 1000:
    traci.simulationStep()
+   
    step += 1
 
 traci.close()
