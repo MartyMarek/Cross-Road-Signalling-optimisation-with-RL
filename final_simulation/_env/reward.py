49a131f88897b7e0aab9e288c73362aba61d9c56
Zachary Kerr <Zachary.Kerr@bentley.com>
2022-05-28 09:34:46 +1000

    incremenal training

--- final_simulation/_env/reward.py
+++ final_simulation/_env/reward.py
@@ -182,3 +182,75 @@ def calculate_reward_09(throughput, cars_waiting, accumulated_wait_time, current
     reward = throughput_reward - cars_waiting_punishment - short_signal_punishment - long_signal_punishment
 
     return reward
+
+def calculate_reward_10(throughput, cars_waiting, accumulated_wait_time, current_signal_state, previous_signal_state,
+                        previous_signal_active_time):
+
+    if cars_waiting > 0:
+        average_wait_time_punishment = (accumulated_wait_time/cars_waiting) * 0.5
+    else:
+        average_wait_time_punishment = 0
+
+    if current_signal_state != previous_signal_state and previous_signal_active_time < 5:
+        short_signal_punishment = 30
+    else:
+        short_signal_punishment = 0
+
+    if current_signal_state == previous_signal_state and previous_signal_active_time > 30:
+        long_signal_punishment = 20
+    else:
+        long_signal_punishment = 0
+
+    throughput_reward = throughput * 6
+    cars_waiting_punishment = cars_waiting
+
+    reward = throughput_reward - cars_waiting_punishment - short_signal_punishment - long_signal_punishment
+
+    return reward
+
+def calculate_reward_11(throughput, cars_waiting, accumulated_wait_time, current_signal_state, previous_signal_state,
+                        previous_signal_active_time, signal_states):
+
+    # Cars waiting punishment
+    cars_waiting_punishment = cars_waiting
+
+    # Average wait time punishment
+    if cars_waiting > 0:
+        average_wait_time_punishment = accumulated_wait_time/cars_waiting
+    else:
+        average_wait_time_punishment = 0
+
+    # Accumulated wait time punishment
+    accumulated_wait_time_punishment = accumulated_wait_time
+
+    # Short signal punishment
+    if current_signal_state != previous_signal_state and previous_signal_active_time < 5:
+        short_signal_punishment = 60
+    else:
+        short_signal_punishment = 0
+
+    # Long signal punishment
+    if current_signal_state == previous_signal_state and previous_signal_active_time > 30:
+        long_signal_punishment = 20
+    else:
+        long_signal_punishment = 0
+
+    # Correct sequence reward
+    if signal_states(previous_signal_state).name == 'rgrrrgrr' and signal_states(current_signal_state).name == 'ryrrryrr':
+        signal_sequence_reward = 40
+    elif signal_states(previous_signal_state).name == 'GrrrGrrr' and signal_states(current_signal_state).name == 'yrrryrrr':
+        signal_sequence_reward = 40
+    elif signal_states(previous_signal_state).name == 'rrrgrrrg' and signal_states(current_signal_state).name == 'rrryrrry':
+        signal_sequence_reward = 40
+    elif signal_states(previous_signal_state).name == 'rrGrrrGr' and signal_states(current_signal_state).name == 'rryrrryr':
+        signal_sequence_reward = 40
+    else:
+        signal_sequence_reward = 0
+
+    # Throughput reward
+    throughput_reward = throughput * 10
+    
+
+    reward = throughput_reward + signal_sequence_reward - accumulated_wait_time_punishment - cars_waiting_punishment - short_signal_punishment - long_signal_punishment
+
+    return reward
